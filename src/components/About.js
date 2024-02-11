cb8defdef333ee2842c4f9fd52d5966c38a0780a
Priyanshu Kumar <113720421+Priyanshu9528@users.noreply.github.com>
2024-02-11 22:35:59 +0530

    About.js

--- src/components/About.js
+++ src/components/About.js
@@ -34,36 +34,36 @@ export default function About() {
     <div className="accordion-item">
       <h2 className="accordion-header" id="headingOne">
         <button className="accordion-button" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Accordion Item #1
+          Convert Text
         </button>
       </h2>
       <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
         <div className="accordion-body" style={myStyle}>
-          <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+          Textutils simplifies text manipulation by offering a range of conversion options. Easily transform your text into uppercase or lowercase with just a click, ensuring consistency and clarity in your documents. Whether you need to shout a message in uppercase or create a more subdued tone in lowercase, Textutils makes it effortless.
         </div>
       </div>
     </div>
     <div className="accordion-item">
       <h2 className="accordion-header" id="headingTwo">
         <button className="accordion-button collapsed"  style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          Accordion Item #2
+          Space Management
         </button>
       </h2>
       <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
         <div className="accordion-body" style={myStyle}>
-          <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+         Say goodbye to cluttered text with Textutils' space management feature. Remove extra spaces from your text with ease, ensuring clean and organized content. Whether you're tidying up a document for presentation or optimizing text for readability, Textutils ensures your message is conveyed effectively without unnecessary distractions.
         </div>
       </div>
     </div>
     <div className="accordion-item">
       <h2 className="accordion-header" id="headingThree">
         <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          Accordion Item #3
+          Text Management and Preview
         </button>
       </h2>
       <div id="collapseThree" className="accordion-collapse collapse" style={myStyle} aria-labelledby="headingThree" data-bs-parent="#accordionExample">
         <div className="accordion-body" style={myStyle}>
-          <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+          Textutils provides convenient text management options alongside a preview feature for a comprehensive editing experience. Easily copy text to clipboard for quick sharing or further editing. Additionally, Textutils offers a preview of your text, allowing you to visualize the final appearance before making any changes. Whether you're fine-tuning a document or preparing content for publication, Textutils empowers you to edit with precision and confidence.
         </div>
       </div>
     </div>
